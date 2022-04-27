1a5eb9a9e89a6217fe2658e3b70a06171ab70eef
Henok Zegeye <henokzegeye1990@gmail.com>
2022-04-27 14:05:47 +0300

    Request test added

---
+++ spec/requests/applicants_spec.rb
@@ -0,0 +1,14 @@
+# require 'rails_helper'
+RSpec.describe 'Applicants', type: :request do
+    
+    describe 'GET #index' do
+        it 'gets all applicants' do
+            3.times {create(:applicant)}
+        
+            get "/applicants", as: :json
+            result = JSON(response.body)
+            
+            expect(result['data'].count).to eq(3)
+        end
+    end
+end
